0507274cb5f5054221d95b1d62618bff44056efb
yots029 <darkflamemaster7070@gmail.com>
2024-06-10 04:17:33 +0900

    upd auto mouse layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/sample/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/sample/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 4
 
 #define TAPPING_TERM 180 // ミリ秒
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5 
